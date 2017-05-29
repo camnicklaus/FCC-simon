78914f99cc2a4fae346ab8a9c1bc96edf5432a09
Cameron Nicklaus <camnicklaus@gmail.com>
2017-05-29 14:26:41 -0700

    create dir for static files fixed server file

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express');
+var path = require('path');
 var port = process.env.PORT || 3000;
-var app = express.createServer();
+var app = express();
 
-app.get('/', function(request, response) {
-    response.sendfile(__dirname + '/index.html');
-}).listen(port);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+}).listen(port, function() {
+    console.log('listening on port ' + port);
+});
