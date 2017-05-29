00ffb3f2d813964b6568534474702f0f1d64cc4b
Cameron Nicklaus <camnicklaus@gmail.com>
2017-05-28 18:17:10 -0700

    added node server file

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var port = process.env.PORT || 3000;
+var app = express.createServer();
+
+app.get('/', function(request, response) {
+    response.sendfile(__dirname + '/index.html');
+}).listen(port);
